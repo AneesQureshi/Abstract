4b2ce126d21a46a3a86c2c2b40c4a6023ef29a9b
Muhammad Anees Qureshi <muhammadanish2011@gmail.com>
2020-03-15 00:33:01 +0530

    added

---
+++ db transfer tool bootstrap/scripts/myjavascript.js
@@ -0,0 +1,88 @@
+﻿
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+	
+      var source=document.getElementById(data);
+	  
+	 
+var position = source.getBoundingClientRect();
+var x1 = position.left;
+var y1 = position.top;
+
+	  var target=ev.target;
+	  
+	  var position = target.getBoundingClientRect();
+var x2 = position.left;
+var y2 = position.top;
+
+	  var colortext=getRandomColor();
+	   var colorbackground=getRandomColor();
+	  
+	ev.target.style.color=colortext;
+	source.style.color=colortext;
+	
+	  /* source.style.backgroundColor = colorbackground;
+	
+	target.style.backgroundColor = colorbackground; */
+	
+	var lineId=document.getElementById("line")
+	connect(source,target,colorbackground,2);
+	
+	
+		
+
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+function getOffset( el ) {
+    var rect = el.getBoundingClientRect();
+    return {
+        left: rect.left + window.pageXOffset,
+        top: rect.top + window.pageYOffset,
+        width: rect.width || el.offsetWidth,
+        height: rect.height || el.offsetHeight
+    };
+}
+
+function connect(div1, div2, color, thickness) { // draw a line connecting elements
+    var off1 = getOffset(div1);
+    var off2 = getOffset(div2);
+    // bottom right
+    var x1 = off1.left + off1.width;
+    var y1 = off1.top + off1.height/2;
+    // top right
+    var x2 = off2.left;
+    var y2 = off2.top+off2.height/2;
+    // distance
+    var length = Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
+    // center
+    var cx = ((x1 + x2) / 2) - (length / 2);
+    var cy = ((y1 + y2) / 2) - (thickness / 2);
+    // angle
+    var angle = Math.atan2((y1-y2),(x1-x2))*(180/Math.PI);
+    // make hr
+    var htmlLine = "<div style='padding:0px; margin:0px; height:" + thickness + "px; background-color:" + color + "; line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);' />";
+    //
+    // alert(htmlLine);
+    document.body.innerHTML += htmlLine;
+}
+	
