902de333adffca75016f533f8afe9d1d7663c6ad
Muhammad Anees Qureshi <muhammadanish2011@gmail.com>
2020-03-15 23:09:34 +0530

    admin mvc lte added

---
+++ adminMvcLte/adminltmvc/adminlteMVC/OLD/AppConfigTransformPublish/Scripts/Validation.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $("input[type='submit']").click(function (e) {
+        if ($("#role").val() === "-1") {
+           e.preventDefault();
+           // display error msg beside ddl too
+      }
+    });
+}
+)
